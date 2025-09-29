0eea2459db8bee861b049197ab8d16f9d7952231
Maxd2026 <maxwell.dolan2026@gbwl.org>
2025-09-29 13:41:26 +0000

    4

--- Main.java
+++ Main.java
@@ -79,6 +79,36 @@ public class Main {
       points = points - 15;
       System.out.println(points);
 
+      // CASTING (convert data types)
+      int originalValue = 5;
+      double castDouble = (double) originalValue;
+      System.out.println("Cast to double: " + castDouble);
+      int castInt = (int) castDouble;
+      System.out.println("Cast back to int: " + castInt);
+
+      // Examples of math and casting
+      System.out.println(3/4); // 0
+      System.out.println(3.0 / 4); // 0.75
+      System.out.println(3 / 4.0); // 0.75
+      System.out.println((double) 3/4);
+      // casting preserved the decimal places (no truncation)
+      System.out.println( (int) 3.0/4 ); // 0 (truncated)
+
+      // Casting is helpful when dividing intergers
+      int apcs = 13;
+      int juniors = 40;
+      double csPercent = (double) apcs/juniors;
+      System.out.println(csPercent); // 0.325
+
+      // ROUNDING by casting
+      double positiveNum = 41.69;
+      double negetiveNum = -67.42;
+      int nearestPositive = (int) (positiveNum + 0.5);
+      int nearestNegetive = (int) (negetiveNum - 0.5);
+      System.out.println(nearestPositive);
+      System.out.println(nearestNegetive);
+
+
 
    }
 }
